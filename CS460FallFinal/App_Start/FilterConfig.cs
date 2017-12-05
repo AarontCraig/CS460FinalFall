9d35b15754576bb60d2e1ac8d820582491f72c68
Aaron Craig <acraig15@wou.edu>
2017-12-05 15:18:49 -0800

    Created default MVC project.

---
+++ CS460FallFinal/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CS460FallFinal
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
