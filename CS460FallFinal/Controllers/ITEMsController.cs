6d6a18f640422f9fb62c5edc70049a59c75e57c8
Aaron Craig <acraig15@wou.edu>
2017-12-05 16:00:18 -0800

    Added CRUD with auto-generated controller for ITEM

---
+++ CS460FallFinal/Controllers/ITEMsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CS460FallFinal.Models;
+
+namespace CS460FallFinal.Controllers
+{
+    public class ITEMsController : Controller
+    {
+        private AuctionContext db = new AuctionContext();
+
+        // GET: ITEMs
+        public ActionResult Index()
+        {
+            var iTEMs = db.ITEMs.Include(i => i.SELLER1);
+            return View(iTEMs.ToList());
+        }
+
+        // GET: ITEMs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ITEM iTEM = db.ITEMs.Find(id);
+            if (iTEM == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iTEM);
+        }
+
+        // GET: ITEMs/Create
+        public ActionResult Create()
+        {
+            ViewBag.SELLER = new SelectList(db.SELLERs, "ID", "NAME");
+            return View();
+        }
+
+        // POST: ITEMs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,NAME,DECRIPTION,SELLER")] ITEM iTEM)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ITEMs.Add(iTEM);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.SELLER = new SelectList(db.SELLERs, "ID", "NAME", iTEM.SELLER);
+            return View(iTEM);
+        }
+
+        // GET: ITEMs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ITEM iTEM = db.ITEMs.Find(id);
+            if (iTEM == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.SELLER = new SelectList(db.SELLERs, "ID", "NAME", iTEM.SELLER);
+            return View(iTEM);
+        }
+
+        // POST: ITEMs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,NAME,DECRIPTION,SELLER")] ITEM iTEM)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(iTEM).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.SELLER = new SelectList(db.SELLERs, "ID", "NAME", iTEM.SELLER);
+            return View(iTEM);
+        }
+
+        // GET: ITEMs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ITEM iTEM = db.ITEMs.Find(id);
+            if (iTEM == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iTEM);
+        }
+
+        // POST: ITEMs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ITEM iTEM = db.ITEMs.Find(id);
+            db.ITEMs.Remove(iTEM);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
